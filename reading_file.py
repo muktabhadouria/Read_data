9a7e0e35487fb9335162fda22d9098118d352435
Mukta Bhadouria <91377180+muktabhadouria@users.noreply.github.com>
2023-03-01 00:02:48 +0530

    Add files via upload

---
+++ reading_file.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import pandas as pd
+
+
+# In[2]:
+
+
+pd.read_csv(r"C:\Users\Mukta\Downloads\countries of the world.csv")
+
+
+# In[3]:
+
+
+import pandas as pd
+
+
+# In[6]:
+
+
+pd.read_table(r"C:\Users\Mukta\Downloads\countries of the world.txt")
+
+
+# In[7]:
+
+
+import pandas as pd
+
+
+# In[8]:
+
+
+pd.read_json(r"C:\Users\Mukta\Downloads\json_sample.json")
+
+
+# In[9]:
+
+
+import pandas as pd
+
+
+# In[10]:
+
+
+pd.read_excel(r"C:\Users\Mukta\Downloads\world_population_excel_workbook.xlsx")
+
+
+# In[ ]:
+
+
+
+
